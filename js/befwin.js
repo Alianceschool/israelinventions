856f88a3e89979c1db9a6d0c0dcbc03c4a967a9b
Alianceschool <163826460+Alianceschool@users.noreply.github.com>
2024-03-18 18:59:48 +0200

    Update befwin.js

--- js/befwin.js
+++ js/befwin.js
@@ -1,6 +1,6 @@
 function checkword(){
     let input1 = document.getElementById("input1").value;
-    if (input1.includes("   ") == true){
+    if (input1.includes("7169046735") == true){
         document.getElementById("input1").style.backgroundColor = "green";
         document.getElementById("nextbutton").id = "visible";
     }
